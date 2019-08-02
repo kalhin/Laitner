99c0fbd45c04630b269e33c8cf4ce605feaa81f8
kalhin <kalginroman@gmail.com>
2019-08-02 19:12:22 +0300

    inplement firebase in project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMRllQTG84kkYy-1VVn7Xy76uCfxavJFg",
+  authDomain: "laither.firebaseapp.com",
+  databaseURL: "https://laither.firebaseio.com",
+  projectId: "laither",
+  storageBucket: "laither.appspot.com",
+  messagingSenderId: "314260058695",
+  appId: "1:314260058695:web:dbed6c2f63e150b0"
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+console.log(firebaseApp.firestore())
+
+// // Get a reference to the storage service, which is used to create references in your storage bucket
+// const storage = firebase.storage();
+// console.log(storage);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
